3ac89e86b116dd879558ff4b7f1294b88d6f14de
unknown <farzin.jabbary@gmail.com>
2020-06-28 23:59:04 -0400

    sanitize input value

--- src/services/search/searchApi.js
+++ src/services/search/searchApi.js
@@ -1,7 +1,7 @@
 import { wrapRequest, xapi } from '../utils';
 
 const getSearches = wrapRequest(async params =>
-    xapi().get(`/api/restaurants?city=${params.city}&address=${params.address}&area=${params.area}&page=${params.page}`)
+    xapi().get(`/api/restaurants?city=${params.city.trim()}&address=${params.address.trim()}&area=${params.area.trim()}&page=${params.page}`)
 
 );
 const getSearch = wrapRequest(async id =>
